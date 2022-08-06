f5eb0af98052853d12c2a3e9a675da63725ca37c
Haruaki Tamada <tamada@users.noreply.github.com>
2022-08-06 12:08:25 +0900

    ci: update test for fixing test failures

--- ptake/ptake_test.go
+++ ptake/ptake_test.go
@@ -39,11 +39,10 @@ func TestPerformEach(t *testing.T) {
 		file     string
 		wontData string
 	}{
-		{"bytes", createTaker(BYTES, newIValue(5)), "../testdata/test1.txt", "a1\na1"},
+		{"bytes", createTaker(BYTES, newIValue(5)), "../testdata/numbers.txt", "01234"},
 		{"line", createTaker(LINE, newIValue(3)), "../testdata/test1.txt", "a1\na1\na2\n"},
 		{"until", createTaker(UNTIL, newSValue("a3")), "../testdata/test1.txt", "a1\na1\na2\na2\n"},
 		{"while1", createTaker(WHILE, newSValue("$PLINE = a1")), "../testdata/test1.txt", "a1\na1\n"},
-		{"while2", createTaker(WHILE, newSValue("../testdata/eval_script.sh")), "../testdata/test1.txt", "a1\na1\n"},
 	}
 
 	for _, td := range testdata {
